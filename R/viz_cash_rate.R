5ffecffe51341cb63a4c5403a901a15472cfc2ca
Zac Gross <izacgross@gmail.com>
2025-04-22 19:20:11 +1000

    Update viz_cash_rate.R

--- R/viz_cash_rate.R
+++ R/viz_cash_rate.R
@@ -161,7 +161,7 @@ for (mt in future_meetings) {
       x        = "Target Rate (%)",
       y        = "Probability (%)"
     ) +
- #   theme_bw() # +
+    theme_bw() # +
 #    theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
   ggsave(
