110f518632782076e521c7ff8be9bc22ed4dc7f9
Zac Gross <izacgross@gmail.com>
2025-04-22 19:33:15 +1000

    Update viz_cash_rate.R

--- R/viz_cash_rate.R
+++ R/viz_cash_rate.R
@@ -155,13 +155,13 @@ for (mt in future_meetings) {
     geom_col(show.legend = FALSE) +
       scale_y_continuous(labels = function(x) sprintf("%.0f%%", x*100))
     +
-    labs(
+  #  labs(
   #    title    = paste("Cash Rate Outcome Probabilities —", format(mt, "%d %B %Y")),
   #    subtitle = paste("As of", format(latest_scrape, "%d %B %Y")),
-      x        = "Target Rate (%)",
-      y        = "Probability (%)"
-    )# +
-    # theme_bw() # +
+  #    x        = "Target Rate (%)",
+  #    y        = "Probability (%)"
+  #  ) +
+    theme_bw()  +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
   ggsave(
