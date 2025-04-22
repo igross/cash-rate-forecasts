b11f125686f22592c928ab6805c46c8676646ba5
Zac Gross <izacgross@gmail.com>
2025-04-22 20:28:51 +1000

    Update viz_cash_rate.R

--- R/viz_cash_rate.R
+++ R/viz_cash_rate.R
@@ -153,14 +153,14 @@ for (mt in future_meetings) {
   # d) create the bar chart
   p <- ggplot(bar_df, aes(factor(bucket), probability, fill = bucket)) +
    geom_col(show.legend = FALSE) +
-  scale_y_continuous(labels = function(x) sprintf("%.0f%%", x*100))
+  scale_y_continuous(labels = function(x) sprintf("%.0f%%", x*100)) +
    
-  #  labs(
-  #    title    = paste("Cash Rate Outcome Probabilities —", format(mt, "%d %B %Y")),
-  #    subtitle = paste("As of", format(latest_scrape, "%d %B %Y")),
-  #    x        = "Target Rate (%)",
-  #    y        = "Probability (%)"
-  #  ) +
+    labs(
+      title    = paste("Cash Rate Outcome Probabilities —", format(mt, "%d %B %Y")),
+      subtitle = paste("As of", format(latest_scrape, "%d %B %Y")),
+      x        = "Target Rate (%)",
+      y        = "Probability (%)"
+    ) +
    theme_bw()  +
    theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
