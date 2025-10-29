9d9eff0ab989fc900a603d714dd7d0332231edb4
Zac Gross <izacgross@gmail.com>
2025-10-29 17:33:03 +1100

    Split heatmap generation into future and past runs

---
+++ R/heat_maps_past.R
@@ -0,0 +1,12 @@
+source(file.path("R", "heat_maps_shared.R"))
+
+past_meeting_filter <- function(meeting_dates, meeting_schedule) {
+  today <- Sys.Date()
+  meetings <- meeting_schedule$meeting_date[(meeting_schedule$meeting_date - 1) < today]
+  sort(unique(as.Date(meetings)))
+}
+
+run_heatmap_pipeline(
+  meeting_filter = past_meeting_filter,
+  run_label = "past meetings"
+)
