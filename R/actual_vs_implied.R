4cc246109519be498b9065892e936942b1f4c254
Zac Gross <izacgross@gmail.com>
2026-04-14 12:41:26 +1000

    Add highlighted latest forecast path to cash rate plot

--- R/actual_vs_implied.R
+++ R/actual_vs_implied.R
@@ -241,6 +241,25 @@ forecast_paths_window <- forecast_paths_window %>%
 latest_path <- forecast_paths_window %>%
   filter(scrape_time == max(scrape_time))
 
+latest_actual_anchor <- plot_actual_filtered %>%
+  filter(date <= min(latest_path$expiry, na.rm = TRUE)) %>%
+  slice_tail(n = 1) %>%
+  transmute(
+    expiry = date,
+    cash_rate = actual_rate,
+    tooltip_text = "Most recent forecast path"
+  )
+
+latest_path_highlight <- latest_path %>%
+  transmute(
+    expiry = expiry,
+    cash_rate = cash_rate,
+    tooltip_text = "Most recent forecast path"
+  ) %>%
+  bind_rows(latest_actual_anchor) %>%
+  arrange(expiry) %>%
+  distinct(expiry, .keep_all = TRUE)
+
 latest_event_date <- forecast_snapshots %>%
   filter(scrape_time %in% kept_scrapes) %>%
   slice_max(event_time, n = 1, with_ties = FALSE) %>%
@@ -264,6 +283,17 @@ latest_event_date <- forecast_snapshots %>%
         text = tooltip_text
       ),
       alpha = 0.5
+    ) +
+    geom_line(
+      data = latest_path_highlight,
+      aes(
+        x = expiry,
+        y = cash_rate,
+        text = tooltip_text
+      ),
+      linewidth = 1,
+      colour = "#000000",
+      linetype = "dashed"
     ) +
     scale_color_gradientn(
       colors = viridis(9),
